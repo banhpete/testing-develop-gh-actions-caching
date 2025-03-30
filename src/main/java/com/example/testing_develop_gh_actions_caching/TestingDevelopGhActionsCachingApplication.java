6b0015073855e3a07eb4a3559522a31597fcebfe
peter <peter.banh95@gmail.com>
2025-03-30 09:40:50 -0400

    init

---
+++ src/main/java/com/example/testing_develop_gh_actions_caching/TestingDevelopGhActionsCachingApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing_develop_gh_actions_caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDevelopGhActionsCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingDevelopGhActionsCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testing_develop_gh_actions_caching/TestingDevelopGhActionsCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_develop_gh_actions_caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingDevelopGhActionsCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
