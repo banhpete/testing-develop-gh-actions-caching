b403515bad99122872aa12228b90cdd41a80d88f
peter <peter.banh95@gmail.com>
2025-03-30 13:47:14 -0400

    Add delete

--- src/main/java/com/example/testing_develop_gh_actions_caching/controllers/InvoiceController.java
+++ src/main/java/com/example/testing_develop_gh_actions_caching/controllers/InvoiceController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,17 @@ public class InvoiceController {
     invoices.add(invoice);
     invoice.setInvoiceId(UUID.randomUUID().toString());
     return ResponseEntity.ok(invoice);
+  }
+
+  @DeleteMapping("/invoices/{id}")
+  public ResponseEntity<Void> deleteInvoice(@PathVariable String id) {
+    Invoice invoice =
+        invoices.stream().filter(i -> i.getInvoiceId().equals(id)).findFirst().orElse(null);
+
+    if (invoice == null) {
+      return ResponseEntity.notFound().build();
+    }
+    invoices.removeIf(i -> i.getInvoiceId().equals(id));
+    return ResponseEntity.noContent().build();
   }
 }
